589de745f0ce26be54909790ef1a3aff34571cca
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-05-14 11:24:38 +0200

    Update: src/index.js, templates/browser_typescript_babel_react.js

--- templates/browser_typescript_babel_react.js
+++ templates/browser_typescript_babel_react.js
@@ -1,8 +1,23 @@
+/**
+ * @file
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
 import { WebpackConfigBuilder } from '@tomaschochola/tooling-webpack';
 
 // eslint-disable-next-line no-restricted-exports
 export default function (env, argv) {
-  let tooling = new WebpackConfigBuilder({ env, argv });
+  let tooling = new WebpackConfigBuilder({
+    env,
+    argv,
+  });
 
   tooling = tooling
     .setEntries({
