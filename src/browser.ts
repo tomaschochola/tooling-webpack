418493718a0c5a20499830553836b3c1db9802ad
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-07-28 14:04:35 +0200

    Add browser source entry point

---
+++ src/browser.ts
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
+export * from './browser-artifacts/browser';
